b2d5c6311e88c792af9aca1b465193550c86716b
Ted Cook <teodoro.cook@gmail.com>
2017-08-29 22:53:39 -0600

    Add support molecule 2.0

--- tests/test_command.py
+++
@@ -1,9 +0,0 @@
-from testinfra.utils.ansible_runner import AnsibleRunner
-
-
-testinfra_hosts = \
-        AnsibleRunner('.molecule/ansible_inventory').get_hosts('test')
-
-
-def test_command(Command):
-    assert Command('tmux -V').rc == 0
