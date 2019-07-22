3258778fa21842ffba42375c6ced4ba8d1ffe898
Ted Cook <teodoro.cook@gmail.com>
2019-07-21 23:11:18 -0600

    Fix testinfra deprecation errors

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -6,5 +6,5 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_command(Command):
-    assert Command('tmux -V').rc == 0
+def test_command(host):
+    assert host.command('tmux -V').rc == 0
